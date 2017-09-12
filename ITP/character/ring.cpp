fe2b759f4361643f25a922c31ee65c5f58c3e394
dantin <chengjie.ding@gmail.com>
2017-09-13 01:33:08 +0800

    refactoring using reference

--- ITP/character/ring.cpp
+++ ITP/character/ring.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-bool match(string *s1, string *s2, int idx)
+bool match(string &s1, string &s2, int idx)
 {
-    for(int i = 0; i < (*s2).length(); i++)
+    for(int i = 0; i < s2.length(); i++)
     {
-        if((*s1)[(i+idx) % (*s1).length()] != (*s2)[i])
+        if(s1[(i+idx) % s1.length()] != s2[i])
             return false;
     }
 
@@ -22,21 +22,13 @@ int main()
     bool found = false;
     for(int i = 0; i < s.length(); i++)
     {
-        if(match(&s, &p, i))
+        if(match(s, p, i))
         {
             found = true;
         }
     }
 
-    if(found)
-    {
-        cout << "Yes";
-    }
-    else
-    {
-        cout << "No";
-    }
-    cout << endl;
+    cout << (found ? "Yes" : "No") << endl;
 
     return 0;
 }
